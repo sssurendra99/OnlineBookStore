19d922329849d54f24f0b9fa7b47707fd081a895
sumalsurendra1999@gmail.com <sumalsurendra1999@gmail.com>
2025-09-03 11:26:54 +0530

    Initial Project Setup

---
+++ backend/book-service/src/test/java/com/bookstore/book/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
